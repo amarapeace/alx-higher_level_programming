8f2c7ec741444aa390adb7c87e10db96858448b3
Onwunzo amara <onwunzoamara@yahoo.com>
2022-07-04 12:02:00 +0000

    data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
-
 def no_c(my_string):
-    new_string = ""
-
-    for letter in range(len(my_string)):
-        if (letter != "c") and (letter != "C"):
-            new_string += letter
-        return new_string
+    chars = len(my_string)
+    buf = ""
+    for i in range(0, chars):
+        if my_string[i] == "c" or my_string[i] == "C":
+            continue
+        buf = buf + my_string[i]
+    return buf
