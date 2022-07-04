59e6c8738c93443e3c6d4cad2c748c2ff186627d
Onwunzo amara <onwunzoamara@yahoo.com>
2022-07-04 10:56:37 +0000

    retrieves an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,4 +4,4 @@ def element_at(my_list, idx):
     for item in my_list:
         if (idx < 0) and (idx >= len(my_list)):
             return none
-        return my_list[idx]
+    return my_list[idx]
